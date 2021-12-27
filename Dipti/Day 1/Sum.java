561b6b735084a64f299ad95a9ec2c3234aba740d
Dipti Chirutkar <76995277+DiptiChirutkar@users.noreply.github.com>
2021-12-27 13:17:35 +0530

    Update Sum.java

--- Dipti/Day 1/Sum.java
+++ Dipti/Day 1/Sum.java
@@ -1,1 +1,12 @@
 
+import java.util.*;
+
+public class Add2Number
+{
+  public static void main (String args[])
+  {
+    int num1= 30, num2=20,sum;
+    sum = num1 + num2;
+    System.out.println("The sum of Two number is:"+sum);
+    }
+}
