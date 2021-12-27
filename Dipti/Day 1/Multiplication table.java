d6b22a227f576db8c55974eae9ba1ffd6444ae90
Dipti Chirutkar <76995277+DiptiChirutkar@users.noreply.github.com>
2021-12-27 16:10:19 +0530

    Update Multiplication table.java

--- Dipti/Day 1/Multiplication table.java
+++ Dipti/Day 1/Multiplication table.java
@@ -1,1 +1,14 @@
 
+import java.util.*;
+
+import java.io.*;
+
+class table
+{
+  public static void main (String args[])
+  {
+    int n = 8;
+    for (int i =1;i<=10;++i)
+    System.out.println(n + "*" + i + "=" + n * i);
+  }
+}
